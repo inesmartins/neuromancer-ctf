48e7d9f35f47011f8f5d84e7109c619ff01364c8
Inês Martins <in.afmartins@gmail.com>
2021-02-13 17:39:55 +0000

    removes req method from response

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const smuggleParam = 'OFLAG'
 http.createServer(function (req, res) {
     if (req.method === "GET") {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>My First Node.js Web Server</h1><p>' + req.method + '</p>');
+        res.end('<h1>My First Node.js Web Server</h1>');
     } else if (req.method === "POST") {
         res.writeHead(400, {'Content-Type': 'text/html', 'Error': smuggleParam});
-        res.end('<h1>Smuggle not found</h1><p>' + req.method + '</p>');
+        res.end('<h1>Smuggle not found</h1>');
     } else if ((req.method === smuggleParam) || (req.headers["Content-Length"] && req.headers["Transfer-Encoding"])) {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>You got it cowboy, flag = oposec{happybdayoposec}</h1><p>' + req.method + '</p>');
+        res.end('<h1>You got it cowboy, flag = oposec{happybdayoposec}</h1>');
     } else {
         res.writeHead(400, {'Content-Type': 'text/html'});
-        res.end('<h1>Yeah, that\'s not gonna fly</h1><p>' + req.method + '</p>');
+        res.end('<h1>Yeah, that\'s not gonna fly</h1>');
     }
 }).listen(PORT);
