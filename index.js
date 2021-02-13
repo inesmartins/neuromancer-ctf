609960b4d57fc4f87683e567127d80c2d38d5a77
Inês Martins <in.afmartins@gmail.com>
2021-02-13 17:09:31 +0000

    add custom reply for post

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ http.createServer(function (req, res) {
     if (req.method === "GET") {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end('<h1>My First Node.js Web Server</h1>');
-    } else if (req.method === "POST" && tranferEncodingHeader && contentLenghtHeader) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        //res.end('<h1>I sense you\'re trying to smuggle something...</h1>');
-        res.end('<h1>You got it cowboy, flag = oposec{happybdayoposec}</h1>');
+    } else if (req.method === "POST") {
+        if (tranferEncodingHeader && contentLenghtHeader) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            //res.end('<h1>I sense you\'re trying to smuggle something...</h1>');
+            res.end('<h1>You got it cowboy, flag = oposec{happybdayoposec}</h1>');
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>I feel like you\'re not trying hard enough</h1>');
+        }
     } else {
         res.writeHead(400, {'Content-Type': 'text/html'});
         res.end('<h1>Yeah, that\'s not gonna fly</h1>');
