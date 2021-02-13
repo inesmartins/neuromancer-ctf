299500d790330828ec93de759b92888c9bbe1aa5
Inês Martins <in.afmartins@gmail.com>
2021-02-13 17:31:45 +0000

    updates

--- index.js
+++ index.js
@@ -2,22 +2,17 @@ var http = require('http');
 const PORT = process.env.PORT || 5000
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<p>' + req.method + '</p><p>' + req.headers.join(' ') + '</p>');
-    /*
     if (req.method === "GET") {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end('<h1>My First Node.js Web Server</h1><p>' + req.method + '</p>');
     } else if (req.method === "POST") {
         res.writeHead(400, {'Content-Type': 'text/html', 'Error':'OFLAG'});
         res.end('<h1>Smuggle not found</h1><p>' + req.method + '</p>');
-    } else if (req.method === "SMUGGLED") {
+    } else if ((req.method === "SMUGGLED") || (req.headers["content-length"] && req.headers["transfer-encoding"])) {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        //res.end('<h1>I sense you\'re trying to smuggle something...</h1>');
         res.end('<h1>You got it cowboy, flag = oposec{happybdayoposec}</h1><p>' + req.method + '</p>');
     } else {
         res.writeHead(400, {'Content-Type': 'text/html'});
         res.end('<h1>Yeah, that\'s not gonna fly</h1><p>' + req.method + '</p>');
     }
-    */
 }).listen(PORT);
