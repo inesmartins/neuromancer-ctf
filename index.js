e10ef9e9a9c703920071861b013cc529504ff331
Inês Martins <in.afmartins@gmail.com>
2021-02-13 17:41:47 +0000

    fixes logic error

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ http.createServer(function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end('<h1>My First Node.js Web Server</h1>');
     } else if (req.method === "POST") {
-        res.writeHead(400, {'Content-Type': 'text/html', 'Error': smuggleParam});
-        res.end('<h1>Smuggle not found</h1>');
-    } else if ((req.method === smuggleParam) || (req.headers["Content-Length"] && req.headers["Transfer-Encoding"])) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>You got it cowboy, flag = oposec{happybdayoposec}</h1>');
+        if ((req.method === smuggleParam) || (req.headers["Content-Length"] && req.headers["Transfer-Encoding"])) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>You got it cowboy, flag = oposec{happybdayoposec}</h1>');
+        } else {
+            res.writeHead(400, {'Content-Type': 'text/html', 'Error': smuggleParam});
+            res.end('<h1>Smuggle not found</h1>');
+        }
     } else {
         res.writeHead(400, {'Content-Type': 'text/html'});
         res.end('<h1>Yeah, that\'s not gonna fly</h1>');
