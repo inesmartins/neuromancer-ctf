3cb8d86e8b41d2b1a08196b839c9659697cb91f3
Inês Martins <in.afmartins@gmail.com>
2021-02-13 20:17:31 +0000

    adds case insentitive validation

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+const user = "CASE";
+const pwd = "WINTERMUTE";
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,11 +14,11 @@ app.get('/', function (req, res) {
     res.set('Content-Type', 'text/html');
     res.send('<!DOCTYPE html><html><body style="background-color:black"><img style="width:350px; position: absolute; top:50px; left: calc(50% - 175px);" src="https://upload.wikimedia.org/wikipedia/en/4/4b/Neuromancer_%28Book%29.jpg"/></body></html>');
 });
+
 app.post('/login', function (req, res) {
     const form_data = req.body;
-    console.log(req.body);
-    if (form_data && form_data.username === "case") {
-        if (form_data.password === "wintermute") {
+    if (form_data && form_data.username.toUpperCase() === user) {
+        if (form_data.password.toUpperCase() === pwd) {
             res.send("here's your flag: oposec{happybday}");
         } else {
             res.send("password is not correct");
