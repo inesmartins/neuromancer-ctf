f169120896a4ac7445900fbb595f1f5eb51a6efc
Inês Martins <in.afmartins@gmail.com>
2021-02-13 16:49:05 +0000

    handles get, post and gpost

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+var http = require('http');
 const PORT = process.env.PORT || 5000
 
-var http = require('http');
-  http.createServer(function (req, res) {
-    if (req.method === "GPOST") {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end('<h1>You got it cowboy, flag = oposec{happybdayoposec}</h1>');
+http.createServer(function (req, res) {
+    if (req.method === "GET") {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>My First Node.js Web Server</h1>');
+    } else if (req.method === "GPOST") {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>You got it cowboy, flag = oposec{happybdayoposec}</h1>');
+    } else {
+        res.writeHead(400, {'Content-Type': 'text/html'});
+        res.end('<h1>Yeah, that\'s not gonna fly</h1>');
     }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>My First Node.js Web Server</h1>');
 }).listen(PORT);
