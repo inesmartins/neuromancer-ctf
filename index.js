4ae6b57a08656e273a11fb89646cc404d9241484
Inês Martins <in.afmartins@gmail.com>
2021-02-13 17:55:52 +0000

    simplifies

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ http.createServer(function (req, res) {
     if (req.method === "GET") {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end('<h1>My First Node.js Web Server</h1>');
+    } else if (req.method === smuggleParam) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end('<h1>You got it cowboy, flag = oposec{happybdayoposec}</h1>');
     } else if (req.method === "POST") {
-        if (req.method === smuggleParam) {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end('<h1>You got it cowboy, flag = oposec{happybdayoposec}</h1>');
-        } else {
-            res.writeHead(400, {'Content-Type': 'text/html', 'Error': smuggleParam});
-            res.end('<h1>Smuggle not found</h1>');
-        }
+        res.writeHead(400, {'Content-Type': 'text/html', 'Error': smuggleParam});
+        res.end('<h1>Smuggle not found</h1>');
     } else {
         res.writeHead(400, {'Content-Type': 'text/html'});
         res.end('<h1>Yeah, that\'s not gonna fly</h1>');
