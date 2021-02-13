3580145a199eb06c0054e85f165bfdb3cd8de3b4
Inês Martins <in.afmartins@gmail.com>
2021-02-13 17:52:19 +0000

    updates

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ http.createServer(function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end('<h1>My First Node.js Web Server</h1>');
     } else if (req.method === "POST") {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>Smuggle not found</h1><p>' + req.headers["Content-Length"] + '</p><p>' + req.headers["Transfer-Encoding"] + '</p>');
+        /*
         if ((req.method === smuggleParam) || (req.headers["Content-Length"] && req.headers["Transfer-Encoding"])) {
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.end('<h1>You got it cowboy, flag = oposec{happybdayoposec}</h1>');
@@ -15,6 +18,7 @@ http.createServer(function (req, res) {
             res.writeHead(400, {'Content-Type': 'text/html', 'Error': smuggleParam});
             res.end('<h1>Smuggle not found</h1>');
         }
+        */
     } else {
         res.writeHead(400, {'Content-Type': 'text/html'});
         res.end('<h1>Yeah, that\'s not gonna fly</h1>');
