e592d67cf2f770836ad7343e276b49ef39b07e24
Inês Martins <in.afmartins@gmail.com>
2021-02-13 17:38:45 +0000

    changes smuggle to oflag

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var http = require('http');
 const PORT = process.env.PORT || 5000
 
+const smuggleParam = 'OFLAG'
+
 http.createServer(function (req, res) {
     if (req.method === "GET") {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end('<h1>My First Node.js Web Server</h1><p>' + req.method + '</p>');
     } else if (req.method === "POST") {
-        res.writeHead(400, {'Content-Type': 'text/html', 'Error':'OFLAG'});
+        res.writeHead(400, {'Content-Type': 'text/html', 'Error': smuggleParam});
         res.end('<h1>Smuggle not found</h1><p>' + req.method + '</p>');
-    } else if ((req.method === "SMUGGLED") || (req.headers["Content-Length"] && req.headers["Transfer-Encoding"])) {
+    } else if ((req.method === smuggleParam) || (req.headers["Content-Length"] && req.headers["Transfer-Encoding"])) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end('<h1>You got it cowboy, flag = oposec{happybdayoposec}</h1><p>' + req.method + '</p>');
     } else {
