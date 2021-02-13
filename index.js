7f081b523c03a5f86a95ca2f1323d39687816766
Inês Martins <in.afmartins@gmail.com>
2021-02-13 16:42:07 +0000

    removes express, moves to barebones http server

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-var app = express()
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var http = require('http');
+  http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Your node.js server is running on localhost:3000');
+}).listen(PORT);
