dbe62b1d711fe4edd88e6452ca3ea9fb5407cbf7
Inês Martins <in.afmartins@gmail.com>
2021-02-13 17:24:38 +0000

    simply replies req

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ var http = require('http');
 const PORT = process.env.PORT || 5000
 
 http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(req);
+    /*
     if (req.method === "GET") {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end('<h1>My First Node.js Web Server</h1><p>' + req.method + '</p>');
     } else if (req.method === "POST") {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>I feel like you\'re not trying hard enough</h1><p>' + req.method + '</p>');
+        res.writeHead(400, {'Content-Type': 'text/html', 'Error':'OFLAG'});
+        res.end('<h1>Smuggle not found</h1><p>' + req.method + '</p>');
     } else if (req.method === "SMUGGLED") {
         res.writeHead(200, {'Content-Type': 'text/html'});
         //res.end('<h1>I sense you\'re trying to smuggle something...</h1>');
@@ -16,4 +19,5 @@ http.createServer(function (req, res) {
         res.writeHead(400, {'Content-Type': 'text/html'});
         res.end('<h1>Yeah, that\'s not gonna fly</h1><p>' + req.method + '</p>');
     }
+    */
 }).listen(PORT);
