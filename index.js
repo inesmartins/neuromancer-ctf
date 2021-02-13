f4d493c72342ae1ecb9410d7db0523d765d85c9f
Inês Martins <in.afmartins@gmail.com>
2021-02-13 17:02:48 +0000

    adds simple request smuggling

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ var http = require('http');
 const PORT = process.env.PORT || 5000
 
 http.createServer(function (req, res) {
+    const tranferEncodingHeader = req.headers["transfer-encoding"];
+    const contentLenghtHeader = req.headers["content-length"]
     if (req.method === "GET") {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end('<h1>My First Node.js Web Server</h1>');
-    } else if (req.method === "GPOST") {
+    } else if (req.method === "POST" && tranferEncodingHeader && contentLenghtHeader) {
         res.writeHead(200, {'Content-Type': 'text/html'});
+        //res.end('<h1>I sense you\'re trying to smuggle something...</h1>');
         res.end('<h1>You got it cowboy, flag = oposec{happybdayoposec}</h1>');
     } else {
         res.writeHead(400, {'Content-Type': 'text/html'});
