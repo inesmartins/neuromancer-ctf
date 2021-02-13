4b5aa848e97e954b933424849bc1c235faf0c0de
Inês Martins <in.afmartins@gmail.com>
2021-02-13 16:46:13 +0000

    returns html

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const path = require('path')
 const PORT = process.env.PORT || 5000
 
 var http = require('http');
   http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Your node.js server is running on localhost:3000');
+    if (req.method === "GPOST") {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end('<h1>You got it cowboy, flag = oposec{happybdayoposec}</h1>');
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>My First Node.js Web Server</h1>');
 }).listen(PORT);
